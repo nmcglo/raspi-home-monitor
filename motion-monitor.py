3eda9c8843c24fa4e29c9672dd917f6881f5f49c
Neil McGlohon <nmcglohon@gmail.com>
2016-09-09 22:49:04 -0400

    added twilio

--- motion-monitor.py
+++ motion-monitor.py
@@ -1,9 +1,31 @@
 from gpiozero import MotionSensor
 from time import sleep
 import datetime
+from twilio.rest import TwilioRestClient
 
-pir = MotionSensor(4)
-while True:
-    if pir.motion_detected:
-        theTime = datetime.datetime.now().time()
-        print("Motion detected! ", theTime)
+ACCOUNT_SID = os.environ['TWILIOSID']
+AUTH_TOKEN = os.environ['TWILIOTOKEN']
+
+client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+
+toNum = os.environ['CELLNUMBER'] #nope!
+fromNum = os.environ['TWILIONUMBER']
+
+
+def send(txtbody):
+    client.messages.create(
+    to = toNum,
+    from = fromNum,
+    body = txtbody
+    )
+
+if __name__ == '__main__':
+
+    pir = MotionSensor(4)
+    while True:
+        if pir.motion_detected:
+            theTime = datetime.datetime.now().time()
+            message = "Motion detected! " + theTime
+            print(message)
+            send(message)
+            sleep(10) #sleep for 30 seconds to avoid repeated alarms
